aae0db569c1cfdf6c9cdbc5dd29fd14d0b250bd4
Liam Bigelow <40188355+bglw@users.noreply.github.com>
2022-08-04 16:34:54 +1200

    Test in the release flow using the release build binary

--- .backstage/version.cjs
+++ .backstage/version.cjs
@@ -2,7 +2,7 @@ const fs = require("fs");
 const path = require("path");
 
 const version = process.env.GIT_VERSION;
-const version_re = /version = "0.0.0"/;
+const version_re = /version\s*[=:]\s*"0.0.0"/;
 
 const err = (m) => {
     console.error(m);
@@ -15,7 +15,7 @@ const file = path.join(__dirname, "../humane/Cargo.toml");
 if (!fs.existsSync(file)) err(`Script expected a file at ${file}`);
 
 let contents = fs.readFileSync(file, { encoding: "utf-8" });
-if (!version_re.test(contents)) err(`Expected file to contain version = "0.0.0"`);
+if (!version_re.test(contents)) err(`Expected file to contain a version of "0.0.0"`);
 
 contents = contents.replace(version_re, `version = "${version}"`);
 fs.writeFileSync(file, contents);
